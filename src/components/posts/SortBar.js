d2dee1ebf4894690d897613142e011c9ad46fcf8
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2023-02-28 18:32:55 +0330

    Sort posts

--- src/components/posts/SortBar.js
+++ src/components/posts/SortBar.js
@@ -1,6 +1,24 @@
+import routerPush from "@/utils/routerPush";
 import { AdjustmentsVerticalIcon } from "@heroicons/react/24/outline";
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+const sortOptions = [
+  { label: "پربازدیدترین", id: "most" },
+  { label: "محبوب ترین", id: "popular" },
+  { label: "جدیدترین", id: "newest" },
+];
 
 const SortBar = () => {
+  const router = useRouter();
+  const [sort, setSort] = useState(router.query.sort || "newest");
+
+  const sortHandler = (id) => {
+    setSort(id);
+    router.query.sort = id;
+    routerPush(router);
+  };
+
   return (
     <div className="bg-white rounded-3xl px-4 flex items-center">
       <div className="flex gap-x-2 items-center ml-4">
@@ -8,9 +26,22 @@ const SortBar = () => {
         <span className="text-gray-700">مرتب سازی :</span>
       </div>
       <ul className="flex items-center gap-x-4">
-        <li className="py-4 cursor-pointer text-gray-700">پر بازدیدترین</li>
-        <li className="py-4 cursor-pointer text-gray-700">محبوب ترین</li>
-        <li className="py-4 cursor-pointer text-gray-700">جدیدترین</li>
+        {sortOptions.map(({ id, label }) => {
+          return (
+            <li
+              key={id}
+              className={`py-4 cursor-pointer text-gray-700 relative ${
+                id === sort ? "text-purple-700 font-bold" : ""
+              }`}
+              onClick={() => sortHandler(id)}
+            >
+              {label}
+              {id === sort && (
+                <span className="h-[3px] bg-purple-700 w-8 rounded absolute right-0 bottom-0"></span>
+              )}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
--- src/pages/blogs/index.js
+++ src/pages/blogs/index.js
@@ -4,6 +4,7 @@ import MobileCategory from "@/components/posts/MobileCategory";
 import SortBar from "@/components/posts/SortBar";
 import DesktopCategory from "@/components/posts/DesktopCategory";
 import http from "@/services/httpService";
+import queryString from "query-string";
 
 export default function BlogPage({ blogsData, postCategories }) {
   return (
@@ -24,18 +25,16 @@ export default function BlogPage({ blogsData, postCategories }) {
   );
 }
 
-export async function getServerSideProps({ req }) {
+export async function getServerSideProps({ req, query }) {
   const { data: result } = await http.get(
-    "/posts?limit=6&page=1",
+    `/posts?${queryString.stringify(query)}`,
     {
       headers: {
         Cookie: req.headers.cookie || "",
       },
     }
   );
-  const { data: postCategories } = await http.get(
-    "/post-category"
-  );
+  const { data: postCategories } = await http.get("/post-category");
   const { data } = result;
 
   return {
